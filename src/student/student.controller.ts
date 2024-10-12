a27c8ff8f980b3226defe40da573b7ce066599d5
elfrida <elfrida.shashkevich@gmail.com>
2024-10-12 14:19:17 +0300

    add controller

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -22,11 +22,71 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { StudentService } from './student.service';
+import { StudentDto } from './student.dto';
 
-@ApiTags('comments')
-@Controller('comments')
+
+
+@ApiTags('student')
+@Controller('student')
 export class StudentController {
   constructor(
     private studentService: StudentService,
   ) {}
+
+  @Post('getOne')
+  @ApiCreatedResponse({ type: StudentDto })
+  async getOne() {
+    const student = await this.studentService.getOne();
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Student found successfully',
+      student
+    };
+  }
+
+  @Post('getRandomStudents')
+  async getRandomStudents() {
+    
+    const students = await this.studentService.getRandomStudents();
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Students found successfully',
+      students
+    };
+  }
+
+  @Post('getStudentEvents')
+  async getStudentEvents(@Param('id') id: string) {
+    const data = await this.studentService.getStudentEvents(id);
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Events found successfully',
+      data
+    };
+  }
+
+  @Post('getStudentRateByCourse')
+  async getStudentRateByCourse(@Param('course') course: string, @Param('level') level: string) {
+    const students = await this.studentService.getStudentEvents();
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Students found successfully',
+      students
+    };
+  }
+
+  @Post('getStudentMoney')
+  async getStudentMoney(@Param('id') id: string) {
+    const money = await this.studentService.getStudentMoney(id);
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Money found successfully',
+      money
+    };
+  }
 }
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,21 +1,39 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
+import { Student } from './student.entity';
+import { Repository } from 'typeorm';
+
+
+@Injectable()
 export class StudentService {
+
+    constructor(
+        @InjectRepository(Student)
+        private studentRepository: Repository<Student>,
+    
+      ) {}
+
     async getOne() {
+
         // IMPLEMENT ME
     }
 
+    // no in front
     async getRandomStudents() {
         // IMPLEMENT ME
     }
 
-    async getStudentEvents() {
+    async getStudentEvents(id: string) {
         // IMPLEMENT ME
     }
 
-    async getStudentRateByCourse() {
+    // no in front
+    async getStudentRateByCourse(id: string, level: string) {
         // IMPLEMENT ME
     }
 
-    async getStudentMoney() {
+    async getStudentMoney(id: string) {
         // IMPLEMENT ME
     }
+    
 }
