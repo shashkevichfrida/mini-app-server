ba1cd2ebf2f7085b482535914dfc879eeb0bc447
JovialGang <333412@niuitmo.ru>
2024-10-12 14:52:06 +0300

    events

---
+++ src/event/event.service.ts
@@ -0,0 +1,13 @@
+export class EventService {
+    async getOne() {
+        // IMPLEMENT ME
+    }
+
+    async getRandomEvent() {
+        // IMPLEMENT ME
+    }
+
+    async getAllEvents() {
+        // IMPLEMENT ME
+    }
+}
