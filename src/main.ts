e7fda9dfdd7db7b6ee2115f327262bad2e0ad649
JovialGang <333412@niuitmo.ru>
2024-10-12 14:20:35 +0300

    рефакторинг

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,5 @@ import {StudentService} from "./student/student.service";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  const studentService = new StudentService('/Users/jovialgang/hackathon/mini-app-server/users.db');
-
 }
 bootstrap();
